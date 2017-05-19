f9018407f69b6f80cc7ccc12051f1dec1e2b71e4
VoTuanNguyen <14110133@student.hcmute.edu.vn>
2017-05-19 21:13:44 +0700

    Update HelloController.java

--- springboot-jsp-master/src/main/java/com/hellokoding/springboot/HelloController.java
+++ springboot-jsp-master/src/main/java/com/hellokoding/springboot/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.hellokoding.springboot.data.adapter;
+import com.hellokoding.springboot.data.ConnectDB;
 
 @Controller
 public class HelloController {
